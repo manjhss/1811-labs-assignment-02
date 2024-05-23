2422f09621961893e747b6dc53e9f48fd0544d37
Surendra <manjhisurendra08@gmail.com>
2024-05-24 00:18:03 +0530

    minor changes

--- src/app/api/auth/[...nextauth]/route.js
+++ src/app/api/auth/[...nextauth]/route.js
@@ -6,6 +6,7 @@ const handler = NextAuth({
 		GithubProvider({
 			clientId: process.env.GITHUB_ID,
 			clientSecret: process.env.GITHUB_SECRET,
+			callbackUrl: process.env.GITHUB_CALLBACK_URL,
 		}),
 	],
 });
